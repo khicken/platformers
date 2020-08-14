aba84400d426b6febc792c66372c0a49a43d9001
kale <kalebkim1939@gmail.com>
2020-08-13 23:56:53 -0700

    bug fixes, console finalized, validator methods working

--- src/Game.java
+++ src/Game.java
@@ -32,23 +32,24 @@ public class Game {
     }
 
     public void createGame() {
-        w.println("Hello, new adventurer! What will be your explorer's name?\n(Letters only; name should be of length of 2-20 characters)");
-        int nameChangeCount = Main.easterEggs.get("nameChangeCount");
+        w.println("Hello sentinel! What will be your explorer's name?\n(Letters only; name should be of length of 2-20 characters)");
+        int nameChangeCount = -1; // set counter to -1 that way no message appears first time name is entered
         do {
-            input = w.getInput(Window.ValidateTypes.ALPHABETIC, null, false);
-            
-            
-            if(nameChangeCount == 8) {
-                w.print("Here, your name is now Ryan, even if you are a female, this game can't detect your gender. Fits you well?");
+            if(nameChangeCount == 5) {
+                w.println("Here, your name is now Finn, even if you are a female, this game can't detect your gender. Fits you well?");
+                input = "Finn";
                 break;
-            } else if(nameChangeCount == 5) w.println("Holy smokes, what's so hard about this? Do you want your name to be set to something?");
+            } else if(nameChangeCount == 4) w.println("Holy smokes, what's so hard about this? Is it that you want your name to be set to something?");
             else if(nameChangeCount == 3) w.println("It's not that hard to pick an ingame name. . .");
             else if(nameChangeCount == 2) w.println("A bit of a typo here, or a bit undecisive?");
-            else System.out.print("That's fine, choose another name!");
-            Main.easterEggs.get("nameChangeCount") =  Main.easterEggs.get("nameChangeCount") + 1;
+            else if(nameChangeCount >= 0) w.println("That's fine, choose another name!"); // ensures message not shown first time (counter -1)
+            input = w.getInput(Window.ValidateTypes.ALPHABETIC, null, false);
+            nameChangeCount++;
         } while(!InputValidator.confirm(w));
         Main.easterEggs.put("nameChangeCount", nameChangeCount);
         player.setName(input);
+        w.print("Well greetings, " + player.getName() + "!");
+        // i need to implement the file of names easter eggs now
     }
 
     public void loadGame() {
